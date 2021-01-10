6a7a7a855cda1222b2a97071eb6c577849faf3c5
Małgorzata Pinior <jagoda_m@poczta.onet.pl>
2021-01-10 22:30:05 +0100

    More exception handled.

--- projekt.erl
+++ projekt.erl
@@ -18,7 +18,7 @@ skladniki(Num) ->
     { 4, 3, 1, 0},
     { 3, 2, 2, 0},
     { 5, 0, 0, 3},
-    { 0, 0, 5, 0} 
+    { 1, 2, 4, 0} 
     }).
 
 %Wyświetlanie menu
@@ -30,22 +30,31 @@ menu() ->
             * kawa z mlekiem   - 2. *
             * cappuccino       - 3. *
             * herbata          - 4. *
-            * mleko            - 5. *
+            * latte            - 5. *
             *************************
-Zakoncz dzialanie, wcisnij: k
+-- Zakoncz dzialanie, wcisnij: k
+-- Wyswietl aktualny stan magazynu, wcisnij: m
 Wybierz numer napoju: ").
+% Gotowe -> Kawiarka
+% zakonczProcesy -> kawiarka
+% DanieCukru -> Cukiernica
+% daCukier -> cukiernica
+
 
 %rozpoczecie 
 start() ->
     Wejscie = spawn(projekt, wejscie, []),
     Magazyn = spawn(projekt, magazyn, [initStanMagazynu()]),
+    % 5 procesów współbieżnych
     Czajnik = spawn(projekt, czajnik,[]),
     Mlynek = spawn(projekt, mlynek,[]),
-    Gotowe = spawn(projekt, zakonczProcesy,[procesySkonczone()]),
     Podgrzewacz = spawn(projekt, podgrzewacz,[]),
     Kubek = spawn(projekt, kubek, []),
-    DanieCukru = spawn(projekt, daCukier, []),
-    Procesor = spawn(projekt, procesor, [Wejscie, Magazyn, Czajnik, Mlynek, Gotowe, Podgrzewacz, Kubek, DanieCukru]),
+    Cukiernica = spawn(projekt, cukiernica, []),
+    Herbaciarka = spawn(projekt, herbatka, []),
+
+    Kawiarka = spawn(projekt, kawiarka,[procesySkonczone()]),
+    Procesor = spawn(projekt, procesor, [Wejscie, Magazyn, Czajnik, Mlynek, Kawiarka, Podgrzewacz, Kubek, Cukiernica, Herbaciarka]),
     Procesor!{init}.
 
 %obsługa wejścia/wyjścia 
@@ -58,12 +67,25 @@ wejscie() ->
         {Id, start} -> 
             menu(),
             Napoj = io:get_line(""),
-            CzyCukier = io:get_line("Ilosc cukru (0/1/2):"),
-            Id!{wybranieNapoju, Napoj, CzyCukier},
-            wejscie();
+            case Napoj == "k\n" of
+                false -> 
+                    case Napoj == "m\n" of
+                        false ->
+                            CzyCukier = io:get_line("Ilosc cukru (0/1/2):"),
+                            Id!{wybranieNapoju, Napoj, CzyCukier},
+                            wejscie();
+                        true ->
+                            Id!{stanMagazynu},
+                            wejscie()
+                    end;
+                true -> Id!{koniec}
+            end;
+
         {przetwarzanie} ->
-            io:format("Prosze czekac, trwa przygotowywanie napoju ~n"),
+            io:format("Prosze czekac, trwa przetwarzanie zapytania ~n"),
             wejscie();
+        {przygotowywyanie} ->
+            io:format("Prosze czekac, trwa przygotowywanie napoju");
         {zakonczenie} ->
             io:format("Napoj gotowy! Uwaga gorace. ~n"),
             wejscie();
@@ -83,73 +105,93 @@ wejscie() ->
         {brakCukru} ->
             io:format("Brak cukru.  ~n"),
             wejscie();
-        {brakCytryny} ->
-            io:format("Brak cytryny.  ~n"),
-            wejscie();
         {brakKubkow} ->
             io:format("Brak kubkow. Nie mozna przygotowac zadnego napoju. Przepraszamy za utrudnienia. ~n"),
-            erlang:error("Zakonczenie dzialania. Przypadek krytyczny - brak kubkow"),
-            wejscie()
+            erlang:error("Zakonczenie dzialania. Przypadek krytyczny - brak kubkow")
     end.
 
-procesor(Wejscie, Magazyn, Czajnik, Mlynek, Gotowe, Podgrzewacz, Kubek, DanieCukru) ->
+procesor(Wejscie, Magazyn, Czajnik, Mlynek, Kawiarka, Podgrzewacz, Kubek, Cukiernica, Herbaciarka) ->
     receive
         {init} ->
             Wejscie!{self(), init},
-            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Gotowe, Podgrzewacz,Kubek, DanieCukru);
+            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Kawiarka, Podgrzewacz,Kubek, Cukiernica, Herbaciarka);
         {wejscieOk} ->
             Magazyn!{self(), init},
-            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Gotowe, Podgrzewacz, Kubek, DanieCukru);
+            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Kawiarka, Podgrzewacz, Kubek, Cukiernica, Herbaciarka);
         {magazynOk} ->
             Wejscie!{self(), start},
-            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Gotowe, Podgrzewacz, Kubek, DanieCukru);
+            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Kawiarka, Podgrzewacz, Kubek, Cukiernica, Herbaciarka);
         {wybranieNapoju, Napoj, CzyCukier} ->
-            Wejscie!{przetwarzanie},
             %zamiana pobranego stringa na int
             %dlaczego tak: to_string zwraca krotkę, pierwszy element to zamieniona liczba, a drugi to reszta. 
             {NapojInt, _} = string:to_integer(Napoj),
             {CukierInt, _} = string:to_integer(CzyCukier),
-            %wyslanie info do magazynu
-            Magazyn!{self(), napoj, NapojInt, CukierInt},
-            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Gotowe, Podgrzewacz, Kubek, DanieCukru);
+
+            if is_integer(CukierInt) and is_integer(NapojInt) and (1 =< NapojInt andalso NapojInt =< 5) and (0 =< CukierInt andalso CukierInt =< 2) ->
+                    %wyslanie info do magazynu
+                    Magazyn!{self(), napoj, NapojInt, CukierInt, Wejscie},
+                    Wejscie!{przetwarzanie},
+                    procesor(Wejscie, Magazyn, Czajnik, Mlynek, Kawiarka, Podgrzewacz, Kubek, Cukiernica, Herbaciarka);
+                true ->
+                    io:format("~n UWAGA! ~n Podano bledne wartosci, zacznijmy od poczatku :) ~n Numery napojow: 1-5 ~n Ilosc kostek cukru: 0-2 ~n"),
+                    Wejscie!{self(), start},
+                    procesor(Wejscie, Magazyn, Czajnik, Mlynek, Kawiarka, Podgrzewacz, Kubek, Cukiernica, Herbaciarka)
+            end;
+
         %Magazyn ma zasoby na dany napoj 
-        {gotowyDoPracy, UsedCukier} ->
+        {gotowyDoPracy, UsedCukier, UsedHerbata} ->
+            Wejscie!{przygotowywanie},
             Czajnik!{self(),gotujWode},
-            Mlynek!{self(),mielKawe},
-            Podgrzewacz!{self(),podgrzejMleko},
             Kubek!{self(),dajKubek},
-            if UsedCukier == 0 -> Gotowe!{bezcukru};
-                true -> DanieCukru!{self(), dajCukier}
+            if UsedHerbata == 3 -> Herbaciarka!{self(),wsypHerbate};
+                true -> Mlynek!{self(),mielKawe},
+                        Podgrzewacz!{self(),podgrzejMleko}
             end,
-            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Gotowe, Podgrzewacz, Kubek, DanieCukru);
+            if UsedCukier == 0 -> Kawiarka!{bezcukru};
+                true -> Cukiernica!{self(), dajCukier}
+            end,
+            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Kawiarka, Podgrzewacz, Kubek, Cukiernica,Herbaciarka);
         {woda, zagotowana} ->
             io:format("Nalewam wode...  ~n"),
-            Gotowe!{self(), woda},
-            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Gotowe, Podgrzewacz, Kubek, DanieCukru);
+            Kawiarka!{self(), woda},
+            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Kawiarka, Podgrzewacz, Kubek, Cukiernica,Herbaciarka);
         {kawa, zmielona} ->
             io:format("Zmielono kawe...  ~n"),
-            Gotowe!{self(), kawa},
-            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Gotowe, Podgrzewacz, Kubek, DanieCukru);
+            Kawiarka!{self(), kawa},
+            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Kawiarka, Podgrzewacz, Kubek, Cukiernica,Herbaciarka);
         {mleko, podgrzane} ->
             io:format("Podgrzano mleko...  ~n"),
-            Gotowe!{self(), mleko},
-            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Gotowe, Podgrzewacz, Kubek, DanieCukru);
+            Kawiarka!{self(), mleko},
+            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Kawiarka, Podgrzewacz, Kubek, Cukiernica,Herbaciarka);
         {kubek, wystawiony} ->
             io:format("Wystawiono kubek...  ~n"),
-            Gotowe!{self(), kubek},
-            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Gotowe, Podgrzewacz, Kubek, DanieCukru);
+            Kawiarka!{self(), kubek},
+            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Kawiarka, Podgrzewacz, Kubek, Cukiernica,Herbaciarka);
         {cukier, wsypany} ->
             io:format("Wsypano cukier...  ~n"),
-            Gotowe!{self(), cukier},
-            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Gotowe, Podgrzewacz, Kubek, DanieCukru);
+            Kawiarka!{self(), cukier},
+            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Kawiarka, Podgrzewacz, Kubek, Cukiernica,Herbaciarka);
+        {herbata, wsypana} ->
+            io:format("Wsypano herbate...  ~n"),
+            Kawiarka!{self(), herbata},
+            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Kawiarka, Podgrzewacz, Kubek, Cukiernica,Herbaciarka);
         {gotowe} ->
-            Gotowe!{inicjalizacjaTab},
+            Kawiarka!{inicjalizacjaTab},
             Wejscie!{zakonczenie},
             timer:sleep(4000),
             Wejscie!{self(), start},
-            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Gotowe, Podgrzewacz, Kubek, DanieCukru);
-        {"k"} -> 
-            exit(self(), "Koniec")
+            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Kawiarka, Podgrzewacz, Kubek, Cukiernica,Herbaciarka);
+        {brakCzegos} ->
+            Wejscie!{self(), start},
+            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Kawiarka, Podgrzewacz, Kubek, Cukiernica,Herbaciarka);
+        {koniec} -> 
+            exit(self(), "Koniec");
+        {stanMagazynu} ->
+            Magazyn!{self(), aktualnyStan},
+            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Kawiarka, Podgrzewacz, Kubek, Cukiernica,Herbaciarka);
+        {wyswietlonoAktualnyStan} ->
+            Wejscie!{self(), start},
+            procesor(Wejscie, Magazyn, Czajnik, Mlynek, Kawiarka, Podgrzewacz, Kubek, Cukiernica,Herbaciarka)
     end.
 
 %opóźniamy proces działania czajnika, ponieważ woda się długo gotuje 
@@ -187,56 +229,72 @@ kubek()->
             kubek()
     end.
 
-daCukier() ->
+cukiernica() ->
     receive
         {Id, dajCukier} ->
             timer:sleep(2000),
             Id!{cukier, wsypany},
-            daCukier()
+            cukiernica()
     end.
 
-zakonczProcesy(Tab) ->
+herbatka() ->
+    receive
+        {Id, wsypHerbate} ->
+            timer:sleep(3000),
+            Id!{herbata, wsypana},
+            herbatka()
+    end.
+
+kawiarka(Tab) ->
     receive
         {inicjalizacjaTab} ->
-            zakonczProcesy(procesySkonczone());
+            kawiarka(procesySkonczone());
+        %herbata dodaje dwa, poniewaz nie uzywa mleka i kawy, a w innych procesach nie uzywa sie herbaty, dlatego moze zostac 5 procesow
+        {Id, herbata} ->
+            Tab1 = [1,1|Tab],
+            case Tab1 == [1,1,1,1,1] of
+                    false -> null;
+                    true -> Id!{gotowe}
+            end,
+            kawiarka(Tab1);
         {bezcukru} ->
             Tab1 = [1|Tab],
-            zakonczProcesy(Tab1);
+            kawiarka(Tab1);
         {Id, woda} ->
             Tab1 = [1|Tab],
             case Tab1 == [1,1,1,1,1] of
                     false -> null;
                     true -> Id!{gotowe}
             end,
-            zakonczProcesy(Tab1);
+            kawiarka(Tab1);
         {Id, kawa} ->
             Tab1 = [1|Tab],
             case Tab1 == [1,1,1,1,1] of
                     false -> null;
                     true -> Id!{gotowe}
             end,
-            zakonczProcesy(Tab1);
+            kawiarka(Tab1);
         {Id, mleko} ->
             Tab1 = [1|Tab],
             case Tab1 == [1,1,1,1,1] of
                     false -> null;
                     true -> Id!{gotowe}
             end,
-            zakonczProcesy(Tab1);
+            kawiarka(Tab1);
         {Id, cukier} ->
             Tab1 = [1|Tab],
             case Tab1 == [1,1,1,1,1] of
                     false -> null;
                     true -> Id!{gotowe}
             end,
-            zakonczProcesy(Tab1);
+            kawiarka(Tab1);
         {Id, kubek} ->
             Tab1 = [1|Tab],
             case Tab1 == [1,1,1,1,1] of
                     false -> null;
                     true -> Id!{gotowe}
             end,
-            zakonczProcesy(Tab1)
+            kawiarka(Tab1)
     end.
 
 magazyn(Stan) ->
@@ -256,7 +314,11 @@ magazyn(Stan) ->
         {Id, stan} ->
             Id!{gotowe},
             magazyn({Woda, Kawa, Mleko, Herbata, Cukier, Kubek});
-        {Id, napoj, NumerNapoju, IloscCukru} ->
+        {Id, aktualnyStan} ->
+            io:format("Woda: ~p~n Kawa: ~p~n Mleko: ~p~n Herbata: ~p~n Cukier: ~p~n Kubek: ~p~n ~n", [Woda, Kawa, Mleko, Herbata, Cukier, Kubek]),
+            Id!{wyswietlonoAktualnyStan},
+            magazyn({Woda, Kawa, Mleko, Herbata, Cukier, Kubek});
+        {Id, napoj, NumerNapoju, IloscCukru, Wejscie} ->
                 %wyciaganie skladnikow danego napoju
                 Skladniki = skladniki(NumerNapoju),
                 UsedWoda = element(1,Skladniki),
@@ -276,54 +338,55 @@ magazyn(Stan) ->
 
                 case WodaLeft<0 of
                     false -> null;
-                    true -> Id!{brakWody},
+                    true -> Wejscie!{brakWody},
                         timer:sleep(3000),
-                        Id!{gotowe},
+                        Id!{brakCzegos},
                         magazyn({Woda, Kawa, Mleko, Herbata, Cukier,  Kubek})
                 end,
 
                 case KawaLeft<0 of
                     false -> null;
-                    true -> Id!{brakKawy},
+                    true -> Wejscie!{brakKawy},
                         timer:sleep(3000),
-                        Id!{gotowe},
+                        Id!{brakCzegos},
                         magazyn({Woda, Kawa, Mleko, Herbata, Cukier,  Kubek})
                 end,
 
                 case MlekoLeft<0 of
                     false -> null;
-                    true -> Id!{brakMleka},
+                    true -> Wejscie!{brakMleka},
                         timer:sleep(3000),
-                        Id!{gotowe},
+                        Id!{brakCzegos},
                         magazyn({Woda, Kawa, Mleko, Herbata, Cukier,  Kubek})
                 end,
 
                 case HerbataLeft<0 of
                     false -> null;
-                    true -> Id!{brakHerbaty},
+                    true -> Wejscie!{brakHerbaty},
                         timer:sleep(3000),
-                        Id!{gotowe},
+                        Id!{brakCzegos},
                         magazyn({Woda, Kawa, Mleko, Herbata, Cukier, Kubek})
                 end,
 
                 case CukierLeft<0 of
                     false -> null;
-                    true -> Id!{brakCukru},
+                    true -> Wejscie!{brakCukru},
                         timer:sleep(3000),
-                        Id!{gotowe},
+                        Id!{brakCzegos},
                         magazyn({Woda, Kawa, Mleko, Herbata, Cukier, Kubek})
                 end,
 
                 case KubekLeft<0 of
                     false -> null;
-                    true -> Id!{brakKubkow},
+                    true -> Wejscie!{brakKubkow},
                         timer:sleep(3000),
-                        Id!{gotowe},
+                        Id!{brakCzegos},
                         magazyn({Woda, Kawa, Mleko, Herbata, Cukier, Kubek})
                 end,
 
                 %info do procesora, że mamy zasoby 
-                Id!{gotowyDoPracy, UsedCukier},
+                Id!{gotowyDoPracy, UsedCukier, UsedHerbata},
                 %aktualizacja magazynu
                 magazyn({WodaLeft, KawaLeft, MlekoLeft, HerbataLeft, CukierLeft, KubekLeft})
     end.
+
