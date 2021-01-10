98d6fdb2f0839c89579edb894caffdae23ee4003
Małgorzata Pinior <jagoda_m@poczta.onet.pl>
2021-01-10 22:37:10 +0100

    Added .gitignore

--- test.erl
+++
@@ -1,9 +0,0 @@
--module(test).
--compile([export_all]).
-
-start() ->
-   Tab = [1,1,1,1,1],
-   case Tab == [1,1,1,1,1] of
-                    false -> ("fałsz");
-                    true -> io:format("Prawda")
-   end.
